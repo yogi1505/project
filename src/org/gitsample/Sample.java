9b81006de3c31f49f37aec63873626174528b9df
yogi1505 <yogipoornachandran@gmail.com>
2021-08-21 21:59:20 +0530

    Updated

--- src/org/gitsample/Sample.java
+++ src/org/gitsample/Sample.java
@@ -8,16 +8,19 @@ public class Sample {
 	public Sample() {
 		super();
 		System.out.println("Answer is above");
+		System.out.println("hi");
 	}
 
 	public void ramSize() {
 
 		System.out.println("Ram : 12gb");
+		System.out.println("hi");
 	}
 
 	public void romSize() {
 
 		System.out.println("R0m : 128gb");
+		System.out.println("hi");
 	}
 
 	public static void main(String[] args) {
