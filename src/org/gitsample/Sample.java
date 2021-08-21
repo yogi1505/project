123cf259065d237bed80fd2ac00d2f549bf02aec
yogi1505 <yogipoornachandran@gmail.com>
2021-08-21 21:31:35 +0530

    task completed

--- src/org/gitsample/Sample.java
+++ src/org/gitsample/Sample.java
@@ -1,8 +1,29 @@
 package org.gitsample;
 
 public class Sample {
+	public void processorName() {
+		System.out.println("SnapDragon 765G");
+	}
+
+	public Sample() {
+		super();
+		System.out.println("Answer is above");
+	}
+
+	public void ramSize() {
+
+		System.out.println("Ram : 12gb");
+	}
+
+	public void romSize() {
+
+		System.out.println("R0m : 128gb");
+	}
 
 	public static void main(String[] args) {
-		System.out.println("hi");
+		Sample k = new Sample();
+		k.processorName();
+		k.ramSize();
+
 	}
 }
